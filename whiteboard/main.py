094e12fa30e3b59353820911a6ba0efc0b3f9149
Matthew Levine <matthewlevine@google.com>
2013-07-31 10:32:05 -0400

    Add user and logout information.

--- whiteboard/main.py
+++ whiteboard/main.py
@@ -3,15 +3,27 @@ import os
 import jinja2
 import webapp2
 
+from google.appengine.api import users
+
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
 class HomeHandler(webapp2.RequestHandler):
   def get(self):
+    user = users.get_current_user()
+    if not user:
+      return self.redirect(users.create_login_url('/'))
+    logout_url = users.create_logout_url('/')
+
+    template_values = {'user': user, 'logout_url': logout_url}
     template = JINJA_ENVIRONMENT.get_template('home.html')
-    self.response.write(template.render())
+    self.response.write(template.render(template_values))
 
   def post(self):
+    user = users.get_current_user()
+    if not user:
+      return self.redirect(users.create_login_url('/'))
+
     message = self.request.get('message')
     self.response.write(message)
 
