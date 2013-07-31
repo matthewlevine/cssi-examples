3d3d611e6b2872214f4ffbaf9b873b0d1c00d16c
Matthew Levine <matthewlevine@google.com>
2013-07-31 11:19:40 -0400

    Save and display messages.

--- whiteboard/main.py
+++ whiteboard/main.py
@@ -3,10 +3,18 @@ import os
 import jinja2
 import webapp2
 
+from google.appengine.ext import ndb
 from google.appengine.api import users
 
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
+
+
+class Message(ndb.Model):
+  created = ndb.DateTimeProperty(auto_now_add=True)
+  content = ndb.TextProperty()
+  user = ndb.UserProperty()
+
 
 class HomeHandler(webapp2.RequestHandler):
   def get(self):
@@ -15,7 +23,10 @@ class HomeHandler(webapp2.RequestHandler):
       return self.redirect(users.create_login_url('/'))
     logout_url = users.create_logout_url('/')
 
-    template_values = {'user': user, 'logout_url': logout_url}
+    messages = Message.query()
+    messages = messages.order(Message.created)
+
+    template_values = {'user': user, 'logout_url': logout_url, 'messages': messages}
     template = JINJA_ENVIRONMENT.get_template('home.html')
     self.response.write(template.render(template_values))
 
@@ -24,8 +35,10 @@ class HomeHandler(webapp2.RequestHandler):
     if not user:
       return self.redirect(users.create_login_url('/'))
 
-    message = self.request.get('message')
-    self.response.write(message)
+    content = self.request.get('message')
+    message = Message(content=content, user=user)
+    message.put()
+    self.get()
 
 routes = [
     ('/', HomeHandler),
