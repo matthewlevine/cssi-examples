d4c0792ff641538beb26b57b0cef212833925cb6
Matthew Levine <matthewlevine@google.com>
2013-07-31 11:34:05 -0400

    Turn on autoescaping.

--- whiteboard/main.py
+++ whiteboard/main.py
@@ -7,7 +7,7 @@ from google.appengine.ext import ndb
 from google.appengine.api import users
 
 JINJA_ENVIRONMENT = jinja2.Environment(
-    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)), autoescape=True)
 
 
 class Message(ndb.Model):
