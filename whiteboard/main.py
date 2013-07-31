788a7175d34b5d8963abc569a8a1b7e2983e4eb9
Matthew Levine <matthewlevine@google.com>
2013-07-31 10:10:17 -0400

    Add webapp2-based WSGI application.

--- whiteboard/main.py
+++ whiteboard/main.py
@@ -1,1 +1,11 @@
-print 'Hello, world!'
+import webapp2
+
+class HomeHandler(webapp2.RequestHandler):
+  def get(self):
+    self.response.write('Hello, world!')
+
+routes = [
+    ('/', HomeHandler),
+]
+
+app = webapp2.WSGIApplication(routes, debug=True)
