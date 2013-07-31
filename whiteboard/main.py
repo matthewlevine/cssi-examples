27240f124c8aa8759c687ebb0acf49b61009434f
Matthew Levine <matthewlevine@google.com>
2013-07-31 11:30:23 -0400

    Fix consistency problem.

--- whiteboard/main.py
+++ whiteboard/main.py
@@ -17,14 +17,18 @@ class Message(ndb.Model):
 
 
 class HomeHandler(webapp2.RequestHandler):
-  def get(self):
+  def get(self, message=None):
     user = users.get_current_user()
     if not user:
       return self.redirect(users.create_login_url('/'))
     logout_url = users.create_logout_url('/')
 
-    messages = Message.query()
-    messages = messages.order(Message.created)
+    message_query = Message.query()
+    message_query = message_query.order(-Message.created)
+    messages = message_query.fetch(100)
+
+    if message and message not in messages:
+      messages.insert(0, message)
 
     template_values = {'user': user, 'logout_url': logout_url, 'messages': messages}
     template = JINJA_ENVIRONMENT.get_template('home.html')
@@ -38,7 +42,7 @@ class HomeHandler(webapp2.RequestHandler):
     content = self.request.get('message')
     message = Message(content=content, user=user)
     message.put()
-    self.get()
+    self.get(message)
 
 routes = [
     ('/', HomeHandler),
