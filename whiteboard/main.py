2a215f7a648007678f1a9b9f0c14f36f33914bf7
Matthew Levine <matthewlevine@google.com>
2013-07-31 10:14:56 -0400

    Add Jinja templates.

--- whiteboard/main.py
+++ whiteboard/main.py
@@ -1,8 +1,15 @@
+import os
+
+import jinja2
 import webapp2
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
 class HomeHandler(webapp2.RequestHandler):
   def get(self):
-    self.response.write('Hello, world!')
+    template = JINJA_ENVIRONMENT.get_template('home.html')
+    self.response.write(template.render())
 
 routes = [
     ('/', HomeHandler),
